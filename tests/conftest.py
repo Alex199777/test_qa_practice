8820ab8d4858ce2b434ba156c7a04888bef1b2d6
Alex199777 <yushko.1996@list.ru>
2026-02-19 12:47:39 +0300

    add yml file for github actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 @pytest.fixture()
 def driver():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--headless')
+    options.add_argument('--window-size=1920,1080')
     browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)
     yield browser
